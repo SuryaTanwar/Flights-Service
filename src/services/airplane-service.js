7d8dfe3ec21fb462012e836b7ff96de0218ed790
Surya Tanwar <forsurya02@gmail.com>
2024-01-11 11:42:51 +0530

    Added get all airplanes api

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -20,6 +20,16 @@ async function createAirplane(data) {
     }
 }
 
+async function getAirplanes() {
+    try {
+        const airplanes = await airplaneRepository.getAll();
+        return airplanes;
+    } catch(error) {
+        throw new AppError("cannot fetch data of all the airplanes", StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports = {
-    createAirplane
+    createAirplane,
+    getAirplanes
 }
