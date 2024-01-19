2c40c3608f3f21bef05726e00cf51c11646f8b95
Surya Tanwar <forsurya02@gmail.com>
2024-01-20 02:57:44 +0530

    FEAT: added create flights api

--- src/migrations/20240116234115-create-flight.js
+++ src/migrations/20240116234115-create-flight.js
@@ -27,7 +27,7 @@ module.exports = {
         allowNull: false,
         references: {
           model: "Airports",
-          key: "code"
+          key: "code" 
         },
         onDelete: "CASCADE",
       },
